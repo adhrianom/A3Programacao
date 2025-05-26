bd63889b32ef941205bfca9c0aba2494e1e86cb1
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-05-26 10:21:23 -0300

    Remoção dos comentários da package "visao" v0.20

--- src/main/java/visao/FrmCadastroCategoria.java
+++ src/main/java/visao/FrmCadastroCategoria.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package visao;
 
-/**
- *
- * @author gabri
- */
 public class FrmCadastroCategoria extends javax.swing.JFrame {
 
     /**
