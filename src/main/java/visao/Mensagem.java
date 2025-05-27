08906fb4bdbe73fc710e03005fa7b3c551191f93
adhrianom <adhrianocanal@gmail.com>
2025-05-27 11:21:19 -0300

    Atualizando a interface de FrmCadastroCategoria e adicionando ações (incompleto) - v0.25

--- src/main/java/visao/Mensagem.java
+++ src/main/java/visao/Mensagem.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package visao;
 
-/**
- *
- * @author gabri
- */
-public class Mensagem {
-    
+public class Mensagem extends Exception {
+    public Mensagem(String mensagem){
+        super(mensagem);
+    }
 }
