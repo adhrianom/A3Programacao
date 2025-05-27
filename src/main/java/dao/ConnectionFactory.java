cf032315d07e86b686132e3ada5874bb83faebfd
Ahron <AhronStoco@gmail.com>
2025-05-26 21:22:15 -0300

    Ediçao do MySQL - v0.23

--- src/main/java/dao/ConnectionFactory.java
+++ src/main/java/dao/ConnectionFactory.java
@@ -6,7 +6,6 @@ public class ConnectionFactory {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/controleestoquea3";
     private static final String USUARIO = "root";
     private static final String SENHA = "unisul@prog3";
-    //Depois tem que substituir quando criado
 
     public static Connection getConnection() throws SQLException {
         try {
