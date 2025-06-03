d93692b91bf92e728feab1cbb87ed4c35ceaeefd
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-06-03 16:10:42 -0300

    Alterações package principal - v0.48

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,20 +1,25 @@
 package principal;
+
 import dao.ConnectionFactory;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 import visao.FrmMenuPrincipal;
+
 public class Principal {
 
     public static void main(String[] args) {
-        
+
         try {
             ConnectionFactory a = new ConnectionFactory();
             a.getConnection();
             System.out.println("Conectado com sucesso!");
-        } catch (Exception e) {
-            System.out.println("Ocorreu um erro na conexão.");
+            // Instancia a interface gráfica.
+            FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
+            // Torna a janela visível.
+            objetotela.setVisible(true);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro na conexão." + JOptionPane.ERROR_MESSAGE);
+            System.out.println("Erro: " + e.getMessage());
         }
-     // Instancia a interface gráfica.
-    FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
-    // Torna a janela visível.
-    objetotela.setVisible(true);
     }
 }
